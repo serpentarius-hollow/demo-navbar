4d309c9bd3dd8a44da3057475eab25a9f965922a
serpentarius <serpentarius.hollow@gmail.com>
2020-11-11 19:50:54 +0700

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item href="#">Home</b-nav-item>
+      <b-nav-item href="#">Tentang Kami</b-nav-item>
+      <b-nav-item href="#">Persyaratan Dokumen</b-nav-item>
+      <b-nav-item-dropdown text="Berita" right>
+        <b-dropdown-item href="#">Promo</b-dropdown-item>
+        <b-dropdown-item href="#">Artikel</b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item-dropdown text="Bantuan & Panduan" right>
+        <b-dropdown-item href="#">Kebijakan Privasi</b-dropdown-item>
+        <b-dropdown-item href="#">Syarat & Ketentuan</b-dropdown-item>
+      </b-nav-item-dropdown>
+      <b-nav-item href="#">Kontak</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+</template>
